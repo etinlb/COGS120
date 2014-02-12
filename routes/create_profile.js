32b0d46c6680885f456b9355ac78e99f36eba25a
Erik <etinlb@gmail.com>
2014-02-12 15:51:44 -0800

    Inserted bootstrap into the profile, everything is broken now but trying to fix

--- routes/create_profile.js
+++ routes/create_profile.js
@@ -1,12 +1,12 @@
 var dorthy = require("../profile.json");
 var temp;
 
-exports.page1 = function(req, res){
+exports.hunter_page1 = function(req, res){
   console.log("OTHER SHIT HAPPEND");
   res.render("create_profile");
 }
 
-exports.page2 = function(req, res){
+exports.hunter_page2 = function(req, res){
   console.log("SOME SHIT HAPPEND");
   console.log(req.query);
   temp = req.query;
@@ -18,21 +18,22 @@ exports.add = function(req, res){
   console.log(temp);
   console.log("trying to add the temp value");
   var new_person = {
-      "name": temp.name,
-      "email": temp.email,
-      "password": temp.password,
-      "phone": temp.phone,
-      "street": temp.street,
-      "state": temp.state,
-      "zip": temp.zip,
-      "commute_dist": temp.commute_dist,
-      "relocate_dist": temp.relocate_dist,
-      "skills": req.query.skills,
-      "interests": req.query.interests,
-      "profession": req.query.profession,
-      "commute_dist": req.query.commute_dist,
-      "relocate_dist": req.query.relocate_dist
-    };
+    "name": temp.name,
+    "email": temp.email,
+    "password": temp.password,
+    "phone": temp.phone,
+    "street": temp.street,
+    "state": temp.state,
+    "zip": temp.zip,
+    "commute_dist": temp.commute_dist,
+    "relocate_dist": temp.relocate_dist,
+    "skills": req.query.skills,
+    "interests": req.query.interests,
+    "profession": req.query.profession,
+    "commute_dist": req.query.commute_dist,
+    "relocate_dist": req.query.relocate_dist
+  };
+  //store it in our wizard of oz database
   dorthy["profiles"].push(new_person);
   res.render("profile", new_person);
 }
--- routes/log_in.js
+++ routes/log_in.js
@@ -1,3 +1,7 @@
 exports.view = function(req, res){
   res.render('log_in');
 }
+
+exports.log_user_in = function(req, res){
+  res.render('log_in');
+}
