fdca3664afa3d3ba99bfeda52c231e2625dab8cf
KT <KT@KTs-MacBook-Air.local>
2014-02-13 16:30:13 -0800

    changing profile stuff

--- routes/profile.js
+++ routes/profile.js
@@ -2,7 +2,7 @@ var data = require('../profile.json')
 
 exports.view = function(req, res){
   
-console.log(data);
+console.log("profile data :: " + req.params.user + " " + data['profiles']['eparreir@ucsd.edu']);
   res.render('profile');
 
   
