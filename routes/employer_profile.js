c7d3c8ea23a6978e548dc6bb805bbb9056299971
vagrant <vagrant@precise32.(none)>
2014-02-14 03:04:03 +0000

    whattttt employer works now yay erik

--- routes/employer_profile.js
+++ routes/employer_profile.js
@@ -2,5 +2,5 @@ var dorthy = require('../employer_prof.json');
 
 exports.view = function(req, res){
   
-  res.render('employer_profile', data['employer_profiles'][req.params.user]);
+  res.render('employer_profile', dorthy['employer_profiles'][req.params.user]);
 }
