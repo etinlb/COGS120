4302f647d3ad80d36ba8058bc354669a16cc6eb9
Erik <etinlb@gmail.com>
2014-02-13 18:49:09 -0800

    Added a way to get into contact with the employer

--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,7 @@
 var data = require('../profile.json')
 
 exports.view = function(req, res){
-  
 	console.log("profile data :: ");
 	console.log(data['profiles'][req.params.user])
-  
-
-  	res.render('profile' , data['profiles'][req.params.user] );
-  
-
+	res.render('profile' , data['profiles'][req.params.user] );
 }
