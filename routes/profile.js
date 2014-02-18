d49a921f54c784b16860c40647073a56b2747ee9
Erik <etinlb@gmail.com>
2014-02-17 18:52:10 -0800

    Full templating in and working on bootstrapafiying the rest

--- routes/profile.js
+++ routes/profile.js
@@ -3,5 +3,6 @@ var data = require('../profile.json')
 exports.view = function(req, res){
 	console.log("profile data :: ");
 	console.log(data['profiles'][req.params.user])
-	res.render('profile' , data['profiles'][req.params.user] );
+  var user = req.params.user; 
+	res.render('profile' ,{'user':user,  'data':data['profiles'][req.params.user]} );
 }
