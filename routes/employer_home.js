dc033bf9a6a8279063026338e2fb4f485aab49db
Erik <etinlb@gmail.com>
2014-02-20 16:54:44 -0800

    Addes the ability to post a job in a very hacked up way. DESIGN TRUMPS FUNCTIONALITY!
    But really we should look into proper session handling, not this url
    bull.

--- routes/employer_home.js
+++ routes/employer_home.js
@@ -1,5 +1,8 @@
 exports.view = function(req, res){
   var user = req.params.user;
+  if(req.query.post_job){
+    res.render('employer_home', {'user':user, suc_msg:'1'});
+  }
   res.render('employer_home', {'user':user});
 
 }
